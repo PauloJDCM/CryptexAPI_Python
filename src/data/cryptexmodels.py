4ce76b84791a2e8402592ce1a14650cba29cbace
Paulo Miranda <paulojdcmiranda@gmail.com>
2023-06-15 08:42:09 +0200

    Restructured db tables

--- src/data/cryptexmodels.py
+++ src/data/cryptexmodels.py
@@ -3,39 +3,33 @@ from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, MappedAsDataclass
 
 uuid_type = Annotated[str, 36]
-username_type = Annotated[str, 25]
+playername_type = Annotated[str, 25]
 solution_type = Annotated[str, 35]
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
     type_annotation_map = {
         uuid_type: String(36),
-        username_type: String(25),
+        playername_type: String(25),
         solution_type: String(35)
     }
 
 
-class ActivePuzzle(Base):
-    __tablename__ = "active_puzzles"
+class PlayerStats(Base):
+    __tablename__ = "player_stats"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id = mapped_column(ForeignKey("users.id"))
-    solution: Mapped[solution_type] = mapped_column(nullable=True)
-
-
-class Leaderboard(Base):
-    __tablename__ = "leaderboard"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id = mapped_column(ForeignKey("users.id"))
+    user_id = mapped_column(ForeignKey("players.id"))
+    games_played: Mapped[int]
+    games_won: Mapped[int]
     score: Mapped[int]
 
 
-class User(Base):
-    __tablename__ = "users"
+class Player(Base):
+    __tablename__ = "players"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     external_id: Mapped[uuid_type]
-    name: Mapped[username_type]
-    active_puzzle: Mapped[ActivePuzzle] = relationship()
-    score: Mapped[Leaderboard] = relationship()
+    name: Mapped[playername_type]
+    active_solution: Mapped[solution_type] = mapped_column(nullable=True)
+    stats: Mapped[PlayerStats] = relationship()
--- src/services/cryptexdb.py
+++ src/services/cryptexdb.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine, insert
-from data.cryptexmodels import Base, User, Leaderboard, ActivePuzzle
+from data.cryptexmodels import Base, Player, PlayerStats
 
 
 class CryptexDB:
@@ -12,13 +12,10 @@ class CryptexDB:
 
     def register_user(self, external_id: str, name: str):
         with self._engine.connect() as conn:
-            user_stmt = insert(User).values(external_id=external_id, name=name)
+            user_stmt = insert(Player).values(external_id=external_id, name=name)
             user_id = conn.execute(user_stmt).inserted_primary_key[0]
 
-            lead_stmt = insert(Leaderboard).values(user_id=user_id, score=0)
-            puzzle_stmt = insert(ActivePuzzle).values(user_id=user_id)
-
-            conn.execute(lead_stmt)
-            conn.execute(puzzle_stmt)
+            stats_stmt = insert(PlayerStats).values(user_id=user_id, games_played=0, games_won=0, score=0)
+            conn.execute(stats_stmt)
 
             conn.commit()
