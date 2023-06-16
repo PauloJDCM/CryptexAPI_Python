77fc1f04d6392b484dae2467666efeece32c0a30
Paulo Miranda <paulojdcmiranda@gmail.com>
2023-06-16 09:50:23 +0200

    Changed field type because pydantic doesn't like range type fields

--- src/data/cryptexdtos.py
+++ src/data/cryptexdtos.py
@@ -6,7 +6,7 @@ class DifficultyOptions(BaseModel):
     class Config:
         allow_mutation = False
 
-    ComplexityRange: range
+    ComplexityRange: list[int]
     Permutations: int
     ScoreMultiplier: int
 
@@ -24,10 +24,10 @@ class Difficulty(IntEnum):
     def get_options(self) -> DifficultyOptions:
         match self:
             case Difficulty.Easy:
-                return DifficultyOptions(ComplexityRange=range(4, 7), Permutations=4, ScoreMultiplier=1)
+                return DifficultyOptions(ComplexityRange=[*range(4, 7)], Permutations=4, ScoreMultiplier=1)
 
             case Difficulty.Medium:
-                return DifficultyOptions(ComplexityRange=range(3, 10), Permutations=6, ScoreMultiplier=4)
+                return DifficultyOptions(ComplexityRange=[*range(3, 10)], Permutations=6, ScoreMultiplier=4)
 
             case Difficulty.Hard:
-                return DifficultyOptions(ComplexityRange=range(0, 100), Permutations=8, ScoreMultiplier=9)
+                return DifficultyOptions(ComplexityRange=[*range(0, 100)], Permutations=8, ScoreMultiplier=9)
