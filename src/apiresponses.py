7716ad5852e0b0810b889e21681fa6c9a90ad357
Paulo Miranda <paulojdcmiranda@gmail.com>
2023-06-20 08:34:29 +0200

    Added immutability to DTOs and API responses

--- src/apiresponses.py
+++ src/apiresponses.py
@@ -2,30 +2,48 @@ from pydantic import BaseModel
 
 
 class Puzzle(BaseModel):
+    class Config:
+        allow_mutation = False
+
     Puzzle: list[str]
     Descriptions: list[str]
 
 
 class CheckResult(BaseModel):
+    class Config:
+        allow_mutation = False
+
     IsCorrect: bool
     TriesLeft: int
 
 
 class LeaderboardEntry(BaseModel):
+    class Config:
+        allow_mutation = False
+
     PlayerName: str
     Score: int
 
 
 class Leaderboard(BaseModel):
+    class Config:
+        allow_mutation = False
+
     Entries: list[LeaderboardEntry] = []
 
 
 class PlayerStanding(BaseModel):
+    class Config:
+        allow_mutation = False
+
     Score: int
     FocusedLeaderboard: Leaderboard
 
 
 class PlayerStats(BaseModel):
+    class Config:
+        allow_mutation = False
+
     GamesPlayed: int
     GamesWon: int
     Score: int
